4dbfed89df768c724b237bc5d65e22fdd7d4a78e
Daniel V <daniel.v.veronez@gmail.com>
2020-09-08 07:51:29 -0300

    Criado Models Criado arquivo de dados local

---
+++ MenorPreco/Model/EstabelecimentoModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenorPreco.Model
+{
+    public class EstabelecimentoModel : DbContext
+    {
+        [Key]
+        public int ID_ESTABELECIMENTO { get; set; }
+        public string NME_ESTABELECIMENTO { get; set; }
+        public string NME_LOGRADOURO { get; set; }
+        public string COD_NUMERO_LOGRADOURO { get; set; }
+        public string NME_COMPLEMENTO { get; set; }
+        public string NME_BAIRRO { get; set; }
+        public int COD_MUNICIPIO_IBGE { get; set; }
+        public string NME_MUNICIPIO { get; set; }
+        public string NME_SIGLA_UF { get; set; }
+        public int COD_CEP { get; set; }
+        public double NUM_LATITUDE { get; set; }
+        public double NUM_LONGITUDE { get; set; }
+    }
+}
---
+++ MenorPreco/Model/ProdutoModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenorPreco.Model
+{
+    public class ProdutoModel : DbContext
+    {
+        [Key]
+        public int COD_GTIN { get; set; }
+        public DateTime DAT_EMISSAO { get; set; }
+        public int COD_TIPO_PAGAMENTO { get; set; }
+        public int COD_PRODUTO { get; set; }
+        public int COD_NCM { get; set; }
+        public int COD_UNIDADE { get; set; }
+        public string DSC_PRODUTO { get; set; }
+        public double VLR_UNITARIO { get; set; }
+
+        //Referencia externa
+        public int ID_ESTABELECIMENTO { get; set; }
+    }
+}
