d7b98b30540f2c24ae607cdfdb965ecfae1af37b
Daniel V <daniel.v.veronez@gmail.com>
2020-09-08 14:11:14 -0300

    Atualização

---
+++ MenorPreco/Database/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using MenorPreco.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenorPreco.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<ProdutoModel> Produtos { get; set; }
+        public DbSet<EstabelecimentoModel> Estabelecimentos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
